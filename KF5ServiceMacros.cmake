e7b140e0da8462a3bcb1ab8b63c8acd5de554fab
Harald Sitter <sitter@kde.org>
2014-11-27 09:54:10 +0100

    make kservice_desktop_to_json warning an author_warning
    this warning is only useful for developers. not having it marked as such
    makes automated output parsing for warnings unnecessarily hard
    
    REVIEW: 121262

--- KF5ServiceMacros.cmake
+++ KF5ServiceMacros.cmake
@@ -10,7 +10,7 @@
 #  kservice_desktop_to_json(plasma_engine_time plasma-dataengine-time.desktop)
 
 function(kservice_desktop_to_json)
-  message(WARNING "kservice_desktop_to_json() is deprecated, use kcoreaddons_desktop_to_json instead")
+  message(AUTHOR_WARNING "kservice_desktop_to_json() is deprecated, use kcoreaddons_desktop_to_json instead")
   if(${ARGC} EQUAL 1)
     message(FATAL_ERROR "Calling kservice_desktop_to_json with only one argument is no longer possible. The code should be changed to pass the target which depends on the json file as first argument.")
   endif()
