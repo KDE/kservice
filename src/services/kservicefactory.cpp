b0054f5b5a70b91add58b7672040d2bb72bf7f63
David Faure <faure@kde.org>
2019-12-14 12:13:16 +0100

    Relicense some files to LGPLv2+ or v2v3, OK by all contributors according to relicensecheck.pl
    Summary:
    Unfortunately this doesn't cover all LGPLv2-only files.
    I cannot find a way to contact Bernhard Loos
    (and didn't get a reply from Martijn Klingens)
    
    NO_CHANGELOG
    
    Test Plan: Builds
    
    Reviewers: apol, cgiboudeaux
    
    Reviewed By: cgiboudeaux
    
    Subscribers: cgiboudeaux, kde-frameworks-devel
    
    Tags: #frameworks
    
    Differential Revision: https://phabricator.kde.org/D25975

--- src/services/kservicefactory.cpp
+++ src/services/kservicefactory.cpp
@@ -1,9 +1,11 @@
 /*  This file is part of the KDE libraries
  *  Copyright (C) 1999-2006 David Faure <faure@kde.org>
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Library General Public
- *  License version 2 as published by the Free Software Foundation;
+ *  This library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License or ( at
+ *  your option ) version 3 or, at the discretion of KDE e.V. ( which shall
+ *  act as a proxy as in section 14 of the GPLv3 ), any later version.
  *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
