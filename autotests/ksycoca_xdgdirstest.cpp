3dc221953d5f37e26de87b09dce770cd79dbb89d
David Faure <faure@kde.org>
2020-02-01 23:59:04 +0100

    autotests: simplify test to use bin/data/menus
    The copy in ~/.qttest/config/ can interfer with other tests

--- autotests/ksycoca_xdgdirstest.cpp
+++ autotests/ksycoca_xdgdirstest.cpp
@@ -93,13 +93,9 @@ void KSycocaXdgDirsTest::testOtherAppDir()
 
     const QString appDir = dataDir + "/applications";
 
-    // Copy applications.menus into GenericConfigLocation/menus, so we don't rely on /etc/xdg
-    qputenv("XDG_CONFIG_DIRS", "/doesnexist");
-    const QString menuFile = QFINDTESTDATA("../src/applications.menu");
-    QVERIFY(!menuFile.isEmpty());
-    const QString menuDir = QStandardPaths::writableLocation(QStandardPaths::GenericConfigLocation) + "/menus";
-    QVERIFY(QDir().mkpath(menuDir));
-    QFile::copy(menuFile, menuDir + QLatin1String("/applications.menu"));
+    // The buildsystem copied applications.menus into bin/data/menus, so we don't rely on /etc/xdg
+    const QByteArray modifiedConfigDirs = QFile::encodeName(QCoreApplication::applicationDirPath()) + "/data";
+    qputenv("XDG_CONFIG_DIRS", modifiedConfigDirs);
 
     // test_app_other: live in a different application directory
     const QString testAppOther = appDir + "/test_app_other.desktop";
