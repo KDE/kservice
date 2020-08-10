6a43de4dfecdaf6a423f215b7fde984dc8884e65
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2020-08-10 15:22:59 +0000

    KService: Convert copyright statements to SPDX expressions

--- src/services/kplugininfo.cpp
+++ src/services/kplugininfo.cpp
@@ -1,21 +1,9 @@
-/*  This file is part of the KDE project
-    Copyright (C) 2003,2007 Matthias Kretz <kretz@kde.org>
-    Copyright 2013 Sebastian Kügler <sebas@kde.org>
+/*
+    This file is part of the KDE project
+    SPDX-FileCopyrightText: 2003, 2007 Matthias Kretz <kretz@kde.org>
+    SPDX-FileCopyrightText: 2013 Sebastian Kügler <sebas@kde.org>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License version 2 as published by the Free Software Foundation.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
-
+    SPDX-License-Identifier: LGPL-2.0-only
 */
 
 #include "kplugininfo.h"
